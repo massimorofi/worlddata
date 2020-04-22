ea6c8f35b4cececdbf942430bf5001c83b4b89b4
Rofi <massimo.rofi@gmail.com>
2020-04-22 20:46:08 +0200

    trying to fix cors

--- src/js/routing/RemoteLoader.ts
+++ src/js/routing/RemoteLoader.ts
@@ -10,17 +10,18 @@ export class RemoteLoader {
         // console.log(window.location);
         xobj.open('GET', url, true);
         xobj.overrideMimeType(mimeType);
-        //xobj.setRequestHeader('Access-Control-Allow-Credentials', 'true');
-        //xobj.setRequestHeader('Access-Control-Allow-Origin', '*');
-        //xobj.setRequestHeader('Access-Control-Allow-Methods', 'GET');
-        // xobj.setRequestHeader('Access-Control-Allow-Headers', 'Origin,Access-Control-Allow-Origin,Access-Control-Allow-Methods,X-Requested-With,Access-Control-Allow-Headers,Content-Type,Authorization');
-        //xobj.setRequestHeader("Content-Type", mimeType);
-        //xobj.setRequestHeader('Origin', 'ITRMIGSPRI382BA');
+        xobj.setRequestHeader('Access-Control-Allow-Credentials', 'true');
+        xobj.setRequestHeader('Access-Control-Allow-Origin', '*');
+        xobj.setRequestHeader('Access-Control-Allow-Methods', 'GET');
+        xobj.setRequestHeader('Access-Control-Allow-Headers', 'Origin,Access-Control-Allow-Origin,Access-Control-Allow-Methods,X-Requested-With,Access-Control-Allow-Headers,Content-Type,Authorization');
+        xobj.setRequestHeader("Content-Type", mimeType);
+        //xobj.setRequestHeader('Origin', '*');
         xobj.onreadystatechange = function () {
             if (xobj.readyState == 4 && xobj.status == 200) {
                 func(xobj);
             }
         };
         xobj.send(null);
+        //console.log(xobj.getAllResponseHeaders());
     }
 }
